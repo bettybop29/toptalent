605a2d5741ba8763f050d3a047419404b388f492
Fauziah Aulia <fauziahaulia21@gmail.com>
2022-05-17 02:23:38 +0700

    ui done

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,57 +1,58 @@
 <template>
 <b-container fluid class="bv-example-row">
   <b-row>
-    <b-col>
+    <b-col md="6">
         <img class="image-login" src="../assets/signup-image.png" alt="">
         <div>
           <b-card            
             tag="article"
-            style="max-width: 20rem;"
-            class="mb-2 card-testimony"
-          >
-            <b-card-text>
-              “I’ve been using this incredible job portal. They have comprehensive features that can help me build people in my company. 
-              Now, I’m building my business much better than before. Thanks toptalent!”
-              <br>
-              Jane Doe <br>
-              Founder, PT Karya Bangsa Nasional
+            class="mb-2 card-testimony">
+            <b-card-text class="text-testimony">
+              <p>
+                “I’ve been using this incredible job portal. They have comprehensive features that can help me build people in my company. 
+                Now, I’m building my business much better than before. Thanks toptalent!”
+              </p>
+              
+              
+              <h5>Jane Doe</h5> 
+              <h6>Founder, PT Karya Bangsa Nasional</h6>
+              <font-awesome-icon icon="fa-solid fa-chevron-right" />
             </b-card-text>
-
           </b-card>
         </div>
     </b-col>
-    <b-col>
+    <b-col md="6">
         <div class="container">
         <h1>Start building your Top Company!</h1>
         <div class="form-login">
         <form action="" @submit.prevent="login">
-                <label for="validationDefault01" class="form-label mt-4"
+                <label for="email" class="form-label mt-4"
                   >Email</label
                 >
                 <input
                   type="email"
                   v-model="email"
                   class="form-control"
-                  id="floatingInput"
+                  id="email"
                   aria-describedby="emailHelp"
                   placeholder="yourcompany@mail.com"
                   required
                 />
                 
-                <label for="floatingInput" class="form-label mt-3"
+                <label for="password" class="form-label mt-3"
                   >Password</label
                 >
                 <input
                   type="password"
                   v-model="password"
                   class="form-control"
-                  id="myInput"
+                  id="password"
                   placeholder="Password123@"
                   required
                   
                 />
                 <div class="password-required">
-                  <p class="password-required">Password required:</p>
+                  <p>Password required:</p>
                   <ul>
                     <li>8 characters</li>
                     <li>1 upper-case [A-Z]</li>
@@ -84,9 +85,9 @@
                   
                 />
                 
-                 <input type="submit" class="btn btn-primary" :disabled="searchDisabled" value="Create Account">
+                 <input type="submit" class="btn btn-primary btn-signup" :disabled="searchDisabled" value="Create Account">
                 <p class="sign-up mt-4">Already have an account? 
-                    <router-link class="btn-signup" to="/login">Log In</router-link>
+                    <router-link class="btn-login" to="/login">Log In</router-link>
                     </p>
                  
               </form>
@@ -99,7 +100,9 @@
 </template>
 
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import "mosha-vue-toastify/dist/style.css";
+import { createToast } from "mosha-vue-toastify";
 
 export default {
     name:'SignUpView',
@@ -111,19 +114,41 @@ export default {
       }
     },
     methods:{
-      async signUp(){
-        let response = '';
-        try {
-          response = await axios.post(`http://54.255.4.75:9091/api/v1/auth/recruiter/login?recruiterEmail=${this.email}&recruiterPassword=${this.password}`)
-        } catch(err) {
-          this.err = err.response.data.message
-          console.log(err.response.data.message)
-        }
-        if(response.status == 200){
-          console.log(response)
-          localStorage.setItem("user-info", JSON.stringify(response.data.data.registerDTO));
-        }
+      async signUp() {
+    
+      let response = '';
+      try {
+        
+        this.searchDisabled = true;
+        // const passwordCheck = this.recruiterPassword.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*)(+=._-]{8,}$/)
+        // if (passwordCheck != null){
+       
+        response = await axios.post(
+          `http://54.255.4.75:9091/api/v1/auth/recruiter/register?recruiterEmail=${this.recruiterEmail}&recruiterPassword=${this.recruiterPassword}&recruiterCompany=${this.recruiterCompany}&recruiterIndustry=${this.recruiterIndustry}`);
+           
+          
+        // }else {
+        //    createToast("Password must contain at least one number, one capital letter and one special character", {type: "danger"} )
+        //  }
+            
+      } catch (err) {
+        this.err = err.response.data.message
+        this.searchDisabled = false;
+        // console.log(this.searchDisabled)
+        // console.log(err.response.data.errorCode)
+        // createToast(`${err.response.data.message}`, { type: "danger" });
+
       }
+      if(response.status == 200){
+        this.searchDisabled = true;
+        console.log(this.searchDisabled)
+        this.ress = response.status
+        console.log(this.ress)
+        createToast(`Signup Sukses`, { type: "success" });
+        localStorage.setItem("sign-info", JSON.stringify(response.data));
+        this.$router.push('/activation')
+      }
+    },
     },
     
 }
@@ -163,12 +188,13 @@ h1{
 
 .card-testimony{
   box-sizing: border-box;
+  padding: 30px;
 
   position: absolute;
-  width: 570px;
-  height: 402px;
+  width: 550px;
+  max-height: 402px;
   left: 46px;
-  top: 600px;
+  top: 590px;
 
   background: rgba(255, 255, 255, 0.2);
   box-shadow: 40px 50px 4px rgba(0, 0, 0, 0.15);
@@ -177,5 +203,50 @@ h1{
 
   border-radius: 32px;
 }
+
+.text-testimony{
+  color: #fff;
+}
+
+.text-testimony p{
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 32px;
+}
+
+.text-testimony h5{
+margin-top: 80px;
+font-weight: 900;
+font-size: 20px;
+}
+
+.text-testimony h6{
+  font-weight: 700;
+  line-height: 28px;
+}
+
+.password-required p{
+  font-size: 16px;
+  color: #3300FF;
+  margin-top: 12px;
+}
+
+.password-required ul{
+  margin-left: 20px;
+  margin-top: -10px;
+  list-style: none;
+  color: #828282;
+  font-size: 16px;
+}
+
+.password-required ul li::before {
+  content: "\2022";
+  color: #42FF32;
+  font-weight: bold;
+  display: inline-block; 
+  width: 1em;
+  margin-left: -1em;
+}
+
 
 </style>
