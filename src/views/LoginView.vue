2f1d82cb1ade985bd228e0bfa969c152e2f6555a
rizkidani <rizkidanisetiadi@gmail.com>
2022-05-24 10:36:13 +0700

    responsive home+login+signup

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,66 +1,39 @@
 <template>
-  <div class="d-flex">
-    <div class="align-content-center">
-      <img class="image-login" src="../assets/login-image.png" alt="" />
-      <div class="card animate__animated animate__bounceIn">
-        <div class="card-title">
-          <p>
-            “I was freshgraduate until I found toptalent and I find a suitable
-            job here. I can’t imagine what happened to my career without
-            toptalent.oke”
-          </p>
-          <div class="footer">
-            <p class="title-footer">Stephen Wibisono</p>
-            <small>Lead Programmer,<br />Intermedia</small>
+  <div class="container-fluid min-vh-100 d-flex flex-column">
+    <div class="row flex-grow-1">
+      <div class="col-md-6">
+        <img class="image-login img-fluid" src="../assets/login-image.png" alt="">
+        <div class="card mb-2 card-testimony d-none d-md-block d-lg-block">
+          <div class="text-testimony">
+            <p>“I was freshgraduate until I found toptalent and I find a suitable job here.
+              I can’t imagine what happened to my career without toptalent.oke”</p>
+            <div class="footer">
+              <p class="title-footer">Stephen Wibisono</p>
+              <small>Lead Programmer,<br>Intermedia</small>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="p-2 align-content-center">
-      <div class="container animate__animated animate__fadeInDown">
-        <h1>Welcome back!</h1>
+
+      <!-- right panel -->
+      <div class="col-md-6 p-5">
+        <h1 class="animate_animated animate_fadeInDown">Welcome Back!</h1>
         <div class="form-login animate_animated animate_fadeInDown">
           <form action="" @submit.prevent="login">
-            <label for="validationDefault01" class="form-label mt-4"
-              >Email</label
-            >
-            <input
-              type="email"
-              v-model="email"
-              class="form-control"
-              id="floatingInput"
-              aria-describedby="emailHelp"
-              placeholder="yourcompany@mail.com"
-              required
-            />
+            <label for="validationDefault01" class="form-label mt-4">Email</label>
+            <input type="email" v-model="email" class="form-control" id="floatingInput" aria-describedby="emailHelp"
+              placeholder="yourcompany@mail.com" required />
 
             <label for="floatingInput" class="form-label mt-3">Password</label>
-            <input
-              type="password"
-              v-model="password"
-              class="form-control"
-              id="myInput"
-              placeholder="Password123@"
-              required
-            />
-
+            <input type="password" v-model="password" class="form-control mb-3" id="myInput" placeholder="Password123@"
+              required />
             <p>
-              <router-link class="forgot" to="/resetpass"
-                >Forgot Password?</router-link
-              >
+              <router-link class="btn-forgot" to="/resetpass">Forgot Password?</router-link>
             </p>
             <!-- <input class="form-control" type="tel" id="phone" v-model="phone" > -->
-            <input
-              type="submit"
-              class="btn btn-primary"
-              :disabled="searchDisabled"
-              value="Login"
-            />
-            <p class="sign-up mt-4">
-              Don't have any account?
-              <router-link class="forgot" to="/signup"
-                >Sign Up For Free!</router-link
-              >
+            <input type="submit" class="btn btn-primary" :disabled="searchDisabled" value="Login">
+            <p class="sign-up mt-4">Don't have any account?
+              <router-link class="btn-signup" to="/signup">Sign Up For Free!</router-link>
             </p>
           </form>
         </div>
@@ -70,183 +43,153 @@
 </template>
 
 <script>
-import axios from "axios";
-// import intlTelInput from 'intl-tel-input';
-import "intl-tel-input/build/css/intlTelInput.css";
-import "intl-tel-input/build/js/intlTelInput.js";
-// import "mosha-vue-toastify/dist/style.css";
-// import { createToast } from "mosha-vue-toastify";
+  import axios from 'axios'
+  // import intlTelInput from 'intl-tel-input';
+  import 'intl-tel-input/build/css/intlTelInput.css';
+  import 'intl-tel-input/build/js/intlTelInput.js';
+  // import "mosha-vue-toastify/dist/style.css";
+  // import { createToast } from "mosha-vue-toastify";
 
-export default {
-  name: "LoginView",
+  export default {
+    name: 'LoginView',
 
-  data() {
-    return {
-      email: "",
-      password: "",
-      show: false,
-    };
-  },
-  methods: {
-    showThis() {
-      this.show = true;
-    },
-    async login() {
-      let response = "";
-      try {
-        response = await axios.post(
-          `http://54.255.4.75:9091/api/v1/auth/recruiter/login?recruiterEmail=${this.email}&recruiterPassword=${this.password}`
-        );
-      } catch (err) {
-        this.err = err.response.data.message;
-        console.log(err.response.data.message);
-        this.$toast.error(err.response.data.message, {
-          position: "top-right",
-        });
-        // createToast(`${err.response.data.message}`, { type: "danger" });
-      }
-      if (response.status == 200) {
-        console.log(response);
-        localStorage.setItem(
-          "user-info",
-          JSON.stringify(response.data.data.registerDTO)
-        );
-        this.$router.push("/dashboard");
-        this.$toast.success(
-          `Welcome back! ${response.data.data.registerDTO.recruiterCompany}`,
-          {
-            // optional options Object
-            position: "top-right",
-            pauseOnHover: true,
-          }
-        );
-
-        // createToast(`Welcome back!! ${response.data.data.registerDTO.recruiterCompany}`, { type: "success" });
+    data() {
+      return {
+        email: "",
+        password: "",
+        show: false
       }
     },
-  },
-  mounted() {
-    // const input = document.querySelector("#phone");
-    // intlTelInput(input,{
-    //       customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
-    //       return "e.g. " + selectedCountryPlaceholder,selectedCountryData;
-    // },
-    //   preferredCountries:["id"],
-    // })
-    // console.log(input)
-  },
-};
+    methods: {
+      showThis() {
+        this.show = true
+      },
+      async login() {
+        let response = '';
+        try {
+          response = await axios.post(
+            `http://54.255.4.75:9091/api/v1/auth/recruiter/login?recruiterEmail=${this.email}&recruiterPassword=${this.password}`
+            )
+        } catch (err) {
+          this.err = err.response.data.message
+          console.log(err.response.data.message)
+          // createToast(`${err.response.data.message}`, { type: "danger" });
+        }
+        if (response.status == 200) {
+          console.log(response)
+          localStorage.setItem("user-info", JSON.stringify(response.data.data.registerDTO));
+          this.$router.push('/dashboard')
+          // createToast(`Welcome back!! ${response.data.data.registerDTO.recruiterCompany}`, { type: "success" });
+        }
+      }
+    },
+    mounted() {
+      // const input = document.querySelector("#phone");
+      // intlTelInput(input,{
+      //       customPlaceholder: function(selectedCountryPlaceholder, selectedCountryData) {
+      //       return "e.g. " + selectedCountryPlaceholder,selectedCountryData;
+      // },
+      //   preferredCountries:["id"],
+      // })
+      // console.log(input)
+    }
+  }
 </script>
 
 <style scoped>
-* {
-  padding-left: 0;
-  padding-right: 0;
-  font-family: "Montserrat";
-  font-weight: 500;
-}
+  * {
+    /* padding-left: 0;
+    padding-right: 0; */
+    font-family: 'Montserrat';
+    font-weight: 500;
+  }
 
-h1 {
-  font-size: 32px;
-  font-weight: 900;
-  margin-top: 50px;
-}
+  h1 {
+    font-size: 32px;
+    font-weight: 900;
+    margin-top: 50px;
+  }
 
-.section-right {
-  padding: 50px;
-}
-.image-login {
-  /* position: relative; */
-  /* height: 750px; */
-  height: 100vh;
-  /* width: 760px; */
-  /* width: 50vw; */
-}
-.form-control {
-  padding: 10px;
-  background: #eff0f2;
-  color: #838383;
-}
-.btn {
-  margin-top: 100px;
-  width: 100%;
-}
-.sign-up {
-  text-align: center;
-}
+  .section-right {
+    padding: 50px;
+  }
 
-.card-testimony {
-  box-sizing: border-box;
-  padding: 30px;
+  .image-login {
+    /* position: relative; */
+    /* height: 750px; */
+    height: 100%;
+    /* width: 760px; */
+    /* width: 50vw; */
 
-  position: absolute;
-  width: 550px;
-  max-height: 500px;
-  left: 46px;
-  top: 400px;
+  }
 
-  background: rgba(255, 255, 255, 0.2);
-  box-shadow: 40px 50px 4px rgba(0, 0, 0, 0.15);
-  backdrop-filter: blur(11px);
-  position: absolute;
-  width: 500px;
-  height: 320px;
-  top: 390px;
-  left: 120px;
-  /* right: 10px; */
-}
-.card-image {
-  width: 50px;
-  height: auto;
-}
-.footer {
-  margin-top: 60px;
-}
-.title-footer {
-  position: relative;
-  font-weight: bolder;
-}
-small {
-  position: absolute;
-  top: 250px;
-  width: max-content;
-  font-weight: 500;
-}
+  .form-control {
+    padding: 10px;
+    background: #EFF0F2;
+    color: #838383;
+  }
 
-.text-testimony {
-  color: #fff;
-}
+  /* .btn {
+    margin-top: 100px;
+  } */
 
-.text-testimony p {
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 32px;
-}
+  .sign-up {
+    text-align: center;
+  }
 
-.text-testimony .title-footer {
-  margin-top: 50px;
-  font-weight: 900;
-  font-size: 20px;
-}
+  .card-testimony {
+    box-sizing: border-box;
+    padding: 30px;
 
-.text-testimony h6 {
-  font-weight: 700;
-  line-height: 28px;
-}
+    position: absolute;
+    width: 550px;
+    max-height: 500px;
+    left: 345px;
+    top: 560px;
 
-.btn-forgot {
-  text-decoration: none;
-  color: #51aefe;
-}
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 40px 50px 4px rgba(0, 0, 0, 0.15);
+    backdrop-filter: blur(50px);
+    /* Note: backdrop-filter has minimal browser support */
 
-.btn-forgot:hover {
-  text-decoration: underline;
-}
+    border-radius: 32px;
+  }
 
-.btn-signup {
-  text-decoration: none;
-}
-.btn-signup:hover {
-  text-decoration: underline;
-}
+  .text-testimony {
+    color: #fff;
+  }
+
+  .text-testimony p {
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 32px;
+  }
+
+  .text-testimony .title-footer {
+    margin-top: 50px;
+    font-weight: 900;
+    font-size: 20px;
+  }
+
+  .text-testimony h6 {
+    font-weight: 700;
+    line-height: 28px;
+  }
+
+  .btn-forgot {
+    text-decoration: none;
+    color: #51AEFE;
+  }
+
+  .btn-forgot:hover {
+    text-decoration: underline;
+  }
+
+  .btn-signup {
+    text-decoration: none;
+  }
+
+  .btn-signup:hover {
+    text-decoration: underline;
+  }
 </style>
--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,266 +1,251 @@
 <template>
-  <div class="row">
-    <div class="col-md-6">
-        <img class="image-login" src="../assets/signup-image.png" alt="">
-        <div tag="article" class="card mb-2 card-testimony">
+  <div class="container-fluid min-vh-100 d-flex flex-column">
+    <div class="row flex-grow-1">
+      <div class="col-md-6">
+        <img class="img-fluid" src="../assets/signup-image.png" alt="">
+        <div tag="article" class="card mb-2 card-testimony d-none d-md-block d-lg-block">
           <div class="text-testimony">
             <p>
-              “I’ve been using this incredible job portal. They have comprehensive features that can help me build people in my company. 
+              “I’ve been using this incredible job portal. They have comprehensive features that can help me build
+              people in my company.
               Now, I’m building my business much better than before. Thanks toptalent!”
             </p>
-            <h5>Jane Doe</h5> 
+            <h5>Jane Doe</h5>
             <h6>Founder, PT Karya Bangsa Nasional</h6>
           </div>
         </div>
-    </div>
-    <div class="col-md-6 section-right"> 
-      <h1 class="animate__animated animate__fadeInLeft">Start Building Your Top Company!</h1>
-      <div class="form-login">
-      <form action="" @submit.prevent="signUp" class="row">
-        <div class="col-12">
-            <label for="email" class="form-label mt-4">Email</label>
-            <input
-              type="email"
-              v-model="recruiterEmail"
-              class="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              placeholder="yourcompany@mail.com"
-              required
-            />
-        </div>
-        <div v-if="this.err == 'User already exists'">
-              <b-badge variant="danger">{{err}}</b-badge>
+      </div>
+      <div class="col-md-6 p-5">
+        <h1 class="animate__animated animate__fadeInLeft">Start Building Your Top Company!</h1>
+        <div class="form-login">
+          <form action="" @submit.prevent="signUp" class="row">
+            <!-- email field -->
+            <div class="col-12">
+              <label for="email" class="form-label mt-4">Email</label>
+              <input type="email" v-model="recruiterEmail" class="form-control" id="email" aria-describedby="emailHelp"
+                placeholder="yourcompany@mail.com" required />
+              <div v-if="this.err == 'User already exists'">
+                <b-badge variant="danger">{{err}}</b-badge>
+              </div>
             </div>
-        
-        <label for="password" class="form-label mt-3">Password</label>
-        <input
-          type="password"
-          v-model="recruiterPassword"
-          class="form-control"
-          id="password"
-          placeholder="Password123@"
-          required
-        />
-        <div v-if="this.err == 'Password must be at least 8 characters'">
-              <span class="badge bg-danger">{{err}}</span>
-            </div>
-            <div v-if="this.err == 'Password must contain at least one number, one capital letter and one special character'">
-              <span class="badge bg-danger">{{err}}</span>
-            </div>
-        <div class="password-required">
-          <p>Password required:</p>
-          <ul>
-            <li>8 characters</li>
-            <li>1 upper-case [A-Z]</li>
-            <li>1 lower-case [a-z]</li>
-            <li>1 symbol/special character</li>
-          </ul>
-        </div>
 
-        <!-- company field -->
-        <label for="company" class="form-label mt-3">Company</label>
-        <input
-          type="text"
-          v-model="recruiterCompany"
-          class="form-control"
-          id="company"
-          placeholder="Your Company Name"
-          required
-          
-        />
+            <!-- password field -->
+            <div class="col-12">
+              <label for="password" class="form-label mt-3">Password</label>
+              <input type="password" v-model="recruiterPassword" class="form-control" id="password"
+                placeholder="Password123@" required />
+              <div v-if="this.err == 'Password must be at least 8 characters'">
+                <span class="badge bg-danger">{{err}}</span>
+              </div>
+              <div
+                v-if="this.err == 'Password must contain at least one number, one capital letter and one special character'">
+                <span class="badge bg-danger">{{err}}</span>
+              </div>
+              <div class="password-required">
+                <p>Password required:</p>
+                <ul>
+                  <li>8 characters</li>
+                  <li>1 upper-case [A-Z]</li>
+                  <li>1 lower-case [a-z]</li>
+                  <li>1 symbol/special character</li>
+                </ul>
+              </div>
+            </div>
 
-        <!-- Industry field -->
-        <label for="industry" class="form-label mt-3">Industry</label>
-        <input
-          type="text"
-          v-model="recruiterIndustry"
-          class="form-control"
-          id="industry"
-          placeholder="eg: Technology, Financial"
-          required
-          
-        />
-        <button type="submit" class="btn btn-primary btn-signup" :disabled="searchDisabled">Create Account</button>
-          <!-- <input type="submit" class="btn btn-primary btn-signup" :disabled="searchDisabled" value="Create Account"> -->
-        <p class="sign-up mt-4">Already have an account? 
-          <router-link class="btn-login" to="/login">Log In</router-link>
-        </p>
-      </form>
+            <!-- company field -->
+            <div class="col-12">
+              <label for="company" class="form-label mt-3">Company</label>
+              <input type="text" v-model="recruiterCompany" class="form-control" id="company"
+                placeholder="Your Company Name" required />
+            </div>
+            <!-- Industry field -->
+            <div class="col-12">
+
+              <label for="industry" class="form-label mt-3">Industry</label>
+              <input type="text" v-model="recruiterIndustry" class="form-control" id="industry"
+                placeholder="eg: Technology, Financial" required />
+            </div>
+            <button type="submit" class="btn btn-primary btn-signup" :disabled="searchDisabled">Create Account</button>
+            <!-- <input type="submit" class="btn btn-primary btn-signup" :disabled="searchDisabled" value="Create Account"> -->
+            <p class="sign-up mt-4">Already have an account?
+              <router-link class="btn-login" to="/login">Log In</router-link>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
-// import "mosha-vue-toastify/dist/style.css";
-import 'animate.css';
+  import axios from 'axios';
+  // import "mosha-vue-toastify/dist/style.css";
+  import 'animate.css';
 
 
-export default {
-    name:'SignUpView',
-    
-    data(){
-      return{
+  export default {
+    name: 'SignUpView',
+
+    data() {
+      return {
         recruiterEmail: "",
         recruiterPassword: "",
         recruiterCompany: "",
         recruiterIndustry: "",
-        searchDisabled:false,
-        err:"",
-        ress:""
+        searchDisabled: false,
+        err: "",
+        ress: ""
       }
     },
-    methods:{
+    methods: {
       async signUp() {
-    
-      let response = '';
-      try {
-        
-        this.searchDisabled = true;
-        // const passwordCheck = this.recruiterPassword.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*)(+=._-]{8,}$/)
-        // if (passwordCheck != null){
-       
-        response = await axios.post(
-          `http://54.255.4.75:9091/api/v1/auth/recruiter/register?recruiterEmail=${this.recruiterEmail}&recruiterPassword=${this.recruiterPassword}&recruiterCompany=${this.recruiterCompany}&recruiterIndustry=${this.recruiterIndustry}`);
-           
-          
-        // }else {
-        //    createToast("Password must contain at least one number, one capital letter and one special character", {type: "danger"} )
-        //  }
-            
-      } catch (err) {
-        this.err = err.response.data.message
-        this.searchDisabled = false;
-        // console.log(this.searchDisabled)
-        // console.log(err.response.data.errorCode)
-        // createToast(`${err.response.data.message}`, { type: "danger" });
 
-      }
-      if(response.status == 200){
-        // this.searchDisabled = true;
-        // console.log(this.searchDisabled)
-        this.ress = response.status
-        console.log(this.ress)
-        // createToast(`Signup Sukses`, { type: "success" });
-        localStorage.setItem("sign-info", JSON.stringify(response.data));
-        this.$router.push('/activation')
-      }
+        let response = '';
+        try {
+
+          this.searchDisabled = true;
+          // const passwordCheck = this.recruiterPassword.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*)(+=._-]{8,}$/)
+          // if (passwordCheck != null){
+
+          response = await axios.post(
+            `http://54.255.4.75:9091/api/v1/auth/recruiter/register?recruiterEmail=${this.recruiterEmail}&recruiterPassword=${this.recruiterPassword}&recruiterCompany=${this.recruiterCompany}&recruiterIndustry=${this.recruiterIndustry}`
+          );
+
+
+          // }else {
+          //    createToast("Password must contain at least one number, one capital letter and one special character", {type: "danger"} )
+          //  }
+
+        } catch (err) {
+          this.err = err.response.data.message
+          this.searchDisabled = false;
+          // console.log(this.searchDisabled)
+          // console.log(err.response.data.errorCode)
+          // createToast(`${err.response.data.message}`, { type: "danger" });
+
+        }
+        if (response.status == 200) {
+          // this.searchDisabled = true;
+          // console.log(this.searchDisabled)
+          this.ress = response.status
+          console.log(this.ress)
+          // createToast(`Signup Sukses`, { type: "success" });
+          localStorage.setItem("sign-info", JSON.stringify(response.data));
+          this.$router.push('/activation')
+        }
+      },
     },
-    },
-    
-}
+
+  }
 </script>
 
 <style scoped>
-
-*{
-    padding-left: 0;
-    padding-right: 0;
+  * {
+    /* padding-left: 0;
+    padding-right: 0; */
     font-family: 'Montserrat';
     font-weight: 500;
-}
+  }
 
-.section-right{
-  padding: 50px;
-}
+  .section-right {
+    padding: 50px;
+  }
 
-.image-login{
-    height: 1024px;
-}
-.form-control{
+  .image-login {
+    height: 100%;
+  }
+
+  .form-control {
     padding: 10px;
     background: #EFF0F2;
     color: #838383;
-}
-.btn{
-    margin-top: 100px;
-}
-.sign-up{
-    text-align: center;
-}
+  }
 
-h1{
+  .btn {
+    margin-top: 100px;
+  }
+
+  .sign-up {
+    text-align: center;
+  }
+
+  h1 {
     font-size: 32px;
     font-weight: 900;
     margin-top: 50px;
     margin-left: -10px;
-}
+  }
 
-.card-testimony{
-  box-sizing: border-box;
-  padding: 30px;
+  .card-testimony {
+    box-sizing: border-box;
+    padding: 30px;
 
-  position: absolute;
-  width: 550px;
-  max-height: 402px;
-  left: 46px;
-  top: 590px;
+    position: absolute;
+    width: 550px;
+    max-height: 500px;
+    left: 345px;
+    top: 560px;
 
-  background: rgba(255, 255, 255, 0.2);
-  box-shadow: 40px 50px 4px rgba(0, 0, 0, 0.15);
-  backdrop-filter: blur(50px);
-  /* Note: backdrop-filter has minimal browser support */
+    background: rgba(255, 255, 255, 0.2);
+    box-shadow: 40px 50px 4px rgba(0, 0, 0, 0.15);
+    backdrop-filter: blur(50px);
+    /* Note: backdrop-filter has minimal browser support */
 
-  border-radius: 32px;
-}
+    border-radius: 32px;
+  }
 
-.text-testimony{
-  color: #fff;
-}
+  .text-testimony {
+    color: #fff;
+  }
 
-.text-testimony p{
-  font-weight: 700;
-  font-size: 20px;
-  line-height: 32px;
-}
+  .text-testimony p {
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 32px;
+  }
 
-.text-testimony h5{
-margin-top: 80px;
-font-weight: 900;
-font-size: 20px;
-}
+  .text-testimony h5 {
+    margin-top: 80px;
+    font-weight: 900;
+    font-size: 20px;
+  }
 
-.text-testimony h6{
-  font-weight: 700;
-  line-height: 28px;
-}
+  .text-testimony h6 {
+    font-weight: 700;
+    line-height: 28px;
+  }
 
-.password-required p{
-  font-size: 16px;
-  color: #3300FF;
-  margin-top: 12px;
-}
+  .password-required p {
+    font-size: 16px;
+    color: #3300FF;
+    margin-top: 12px;
+  }
 
-.password-required ul{
-  margin-left: 20px;
-  margin-top: -10px;
-  list-style: none;
-  color: #828282;
-  font-size: 16px;
-}
+  .password-required ul {
+    margin-left: 20px;
+    margin-top: -10px;
+    list-style: none;
+    color: #828282;
+    font-size: 16px;
+  }
 
-.password-required ul li::before {
-  content: "\2022";
-  color: #42FF32;
-  font-weight: bold;
-  display: inline-block; 
-  width: 1em;
-  margin-left: -1em;
-}
+  .password-required ul li::before {
+    content: "\2022";
+    color: #42FF32;
+    font-weight: bold;
+    display: inline-block;
+    width: 1em;
+    margin-left: -1em;
+  }
 
-.btn-login{
-  text-decoration: none;
-}
+  .btn-login {
+    text-decoration: none;
+  }
 
-.btn-login:hover{
-  text-decoration: underline;
-}
-
-
-/* breakpoints */
+  .btn-login:hover {
+    text-decoration: underline;
+  }
 
 
-
+  /* breakpoints */
 </style>
